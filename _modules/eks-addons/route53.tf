1ae1d840ad268b84b536338eefa2aca472389721
Venkatdevops7-web <venkatdevops72@gmail.com>
2024-07-15 09:05:21 +0530

    Update route53.tf

--- _modules/eks-addons/route53.tf
+++ _modules/eks-addons/route53.tf
@@ -1,6 +1,4 @@
 resource "aws_route53_zone" "cluster" {
   for_each = var.instances
   name = "${try(each.value.root_instance, false) ? var.cluster_domain : join(".", [each.key, var.cluster_domain])}"
-
-depends_on = [ helm_release.external_dns ]
 }
